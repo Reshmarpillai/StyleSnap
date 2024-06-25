2917360b321dd01c6e9538edc3dc653a88b07bde
Reshmarpillai <reshmarofficial83@gmail.com>
2024-06-25 11:41:22 +0530

    new files Description box and RelatedProducts added and Item jsx and product jsx changed

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -3,6 +3,8 @@ import { ShopContext } from "../Context/ShopContext";
 import { useParams } from "react-router-dom";
 import { Breadcrumbs } from "../Components/Braedcrumbs/Breadcrumbs";
 import { ProductDisplay } from "../Components/ProductDisplay/ProductDisplay";
+import { DescriptionBox } from "../Components/DescriptionBox/DescriptionBox";
+import { RelatedProducts } from "../Components/RelatedProducts/RelatedProducts";
 
 export const Product = () => {
   const { all_products } = useContext(ShopContext);
@@ -14,6 +16,8 @@ export const Product = () => {
     <div>
       <Breadcrumbs product={product} />
       <ProductDisplay product={product} />
+      <DescriptionBox />
+      <RelatedProducts product={product} />
     </div>
   );
 };
